7a7a151cc6df6e744728f880e54524190dab92fb
Valentino812 <untarvalen123@gmail.com>
2024-08-30 13:44:45 +0700

    finishing about text and adding entrance animation

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function toggleMenu() { //Function to toggle menu in smartphone
+// Function for entrance animation
+document.addEventListener("DOMContentLoaded", function() { 
+    // Two animations (Fade-in and Slide-in)
+    const fadeElements = document.querySelectorAll('.fade-in, .slide-in');
+
+    fadeElements.forEach((element) => {
+        element.style.opacity = '0'; 
+        setTimeout(() => {
+            element.style.opacity = '1'; 
+        }, 10); 
+    });
+});
+
+// Function to toggle menu in smartphone
+function toggleMenu() { 
     const navbar = document.querySelector('.navbar');
     navbar.classList.toggle('open'); 
 }
